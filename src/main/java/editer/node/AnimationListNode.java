d7bfb57adcbb9b7bddaa9ff6857441a94455f235
SAGASA_main <>
2019-10-23 06:24:38 +0900

    書かなきゃ（焦り

---
+++ src/main/java/editer/node/AnimationListNode.java
@@ -0,0 +1,9 @@
+package editer.node;
+
+import javafx.scene.control.ListView;
+import types.model.AnimationKey;
+
+/** アニメーション */
+public class AnimationListNode extends ListView<AnimationKey> {
+
+}
