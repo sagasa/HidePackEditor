5968e9f5f8b3a9bf1767745ba0a16850194f5fea
SagasaWorkPC <03220322a@gmail.com>
2020-01-05 01:01:18 +0900

    ああああああああああなんでバインド入るのおおおおおお

--- src/main/java/editer/node/model/AnimationListNode.java
+++ src/main/java/editer/node/model/AnimationListNode.java
@@ -3,6 +3,7 @@ package editer.node.model;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+
 import editer.node.EditNode;
 import editer.node.EditPanels;
 import editer.node.EditPanels.EditType;
@@ -28,7 +29,7 @@ import types.model.AnimationType;
 /** Boneのアニメーション編集ノード */
 public class AnimationListNode extends EditNode {
 
-	private Map<AnimationType,ListView<AnimationKey>> editMap = new EnumMap<>(AnimationType.class);
+	private Map<AnimationType, ListView<AnimationKey>> editMap = new EnumMap<>(AnimationType.class);
 
 	public AnimationListNode(Property<IEditData> editValue, DataPath path) {
 		super(editValue, EditType.Bone, path, EditNodeType.Other);
@@ -39,12 +40,12 @@ public class AnimationListNode extends EditNode {
 		tabPane.setLayoutX(1);
 		tabPane.prefWidthProperty().bind(widthProperty().subtract(2));
 		tabPane.setStyle("-fx-background-color: red;");
-		for(AnimationType type:AnimationType.values()) {
+		for (AnimationType type : AnimationType.values()) {
 			ListView<AnimationKey> list = new ListView<>();
 			editMap.put(type, list);
 			list.setPrefWidth(180);
 			list.setCellFactory(getCellFactory());
-			Tab tab = new Tab(type.toString(),list);
+			Tab tab = new Tab(type.toString(), list);
 			tab.setClosable(false);
 			tabPane.getTabs().add(tab);
 		}
@@ -57,12 +58,13 @@ public class AnimationListNode extends EditNode {
 	protected void bind(IEditData data) {
 		super.bind(data);
 		for (AnimationType type : AnimationType.values()) {
-			editMap.get(type).setItems(FXCollections.observableArrayList(((ObservableMap<AnimationType,List<AnimationKey>>)editerProperty).get(type)));
+			editMap.get(type).setItems(FXCollections.observableArrayList(
+					((ObservableMap<AnimationType, List<AnimationKey>>) editerProperty).get(type)));
 		}
 	}
 
-	public Callback<ListView<AnimationKey>,ListCell<AnimationKey>> getCellFactory(){
-		return (list)->new KeyEditCell();
+	public Callback<ListView<AnimationKey>, ListCell<AnimationKey>> getCellFactory() {
+		return (list) -> new KeyEditCell();
 	}
 
 	public class KeyEditCell extends ListCell<AnimationKey> {
@@ -71,42 +73,45 @@ public class AnimationListNode extends EditNode {
 		private ObjectProperty<IEditData> prop = new SimpleObjectProperty<>();
 
 		public KeyEditCell() {
-			prop.bind(itemProperty());
-			Region translate =  EditPanels.makePos3Editer(prop, EditType.AnimationKey, new DataPath("translate"));
-			translate.setPrefWidth(194);
-			bindVisible(translate,selectedProperty());
-
-			Region rotate =  EditPanels.makePos3Editer(prop, EditType.AnimationKey, new DataPath("rotate"));
-			rotate.setPrefWidth(194);
-			bindVisible(rotate,selectedProperty());
-
-			Region scale =  EditPanels.makePos3Editer(prop, EditType.AnimationKey, new DataPath("scale"));
-			scale.setPrefWidth(194);
-			bindVisible(scale,selectedProperty());
-
-
-
-			Region key = new EditNode(prop, EditType.AnimationKey, new DataPath("key"), EditNodeType.Float).setChangeListner((c)->{
-				getListView().getItems().sort(null);
-				getListView().refresh();
+			itemProperty().addListener((v, ov, nv) -> {
+				if(nv!=null)
+					prop.set(nv);
 			});
-			root.getChildren().addAll(key,translate,rotate,scale);
+
+			Region translate = EditPanels.makePos3Editer(prop, EditType.AnimationKey, new DataPath("translate"));
+			translate.setPrefWidth(194);
+			bindVisible(translate, selectedProperty());
+
+			Region rotate = EditPanels.makePos3Editer(prop, EditType.AnimationKey, new DataPath("rotate"));
+			rotate.setPrefWidth(194);
+			bindVisible(rotate, selectedProperty());
+
+			Region scale = EditPanels.makePos3Editer(prop, EditType.AnimationKey, new DataPath("scale"));
+			scale.setPrefWidth(194);
+			bindVisible(scale, selectedProperty());
+
+			Region key = new EditNode(prop, EditType.AnimationKey, new DataPath("key"), EditNodeType.Float)
+					.setChangeListner((c) -> {
+						getListView().getItems().sort(null);
+						getListView().refresh();
+					});
+			root.getChildren().addAll(key, translate, rotate, scale);
 		}
 
-		private void bindVisible(Region node,ObservableValue<Boolean> prop) {
+		private void bindVisible(Region node, ObservableValue<Boolean> prop) {
 			node.managedProperty().bind(prop);
 			node.visibleProperty().bind(prop);
 		}
 
 		@Override
-			public void updateSelected(boolean selected) {
-				super.updateSelected(selected);
-				if(selected) {
-					setHeight(140);
-				}else {
-					setHeight(20);
-				}
+		public void updateSelected(boolean selected) {
+			super.updateSelected(selected);
+			if (selected) {
+				setHeight(140);
+			} else {
+				setHeight(20);
 			}
+		}
 
 		@Override
 		protected void updateItem(AnimationKey data, boolean empty) {
